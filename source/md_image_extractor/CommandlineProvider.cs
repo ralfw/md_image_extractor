70d5d35714afa4c478fc7b20c6f90e633a299fee
Ralf Westphal <info@ralfw.de>
2014-10-08 16:27:58 +0200

    removed bash command; checking cmd line params

--- source/md_image_extractor/CommandlineProvider.cs
+++ source/md_image_extractor/CommandlineProvider.cs
@@ -6,6 +6,12 @@ namespace md_image_extractor
 {
 	class CommandlineProvider {
 		public static string Get_filepath(string[] args) {
+			if (args.Length == 0) {
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.Error.WriteLine ("Usage: [mono] md_image_extractor.exe filename");
+				Console.ResetColor ();
+				Environment.Exit (1);
+			}
 			return args [0];
 		}
 	}
--- source/md_image_extractor/extract.command
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-#Ausführungsrechte setzen mit chmod a+x extract.command
-
-mono md_image_extractor.exe $1
